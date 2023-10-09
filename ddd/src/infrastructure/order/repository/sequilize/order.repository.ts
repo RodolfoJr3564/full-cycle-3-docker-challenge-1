f8e1c9a55a674c79d9da9b633ffb36ff5cf7bc70
Rodolfo <rodolfojr94@gmail.com>
2023-10-09 08:55:04 -0300

    feat(ddd): :white_check_mark: Implement order find

--- ddd/src/infrastructure/order/repository/sequilize/order.repository.ts
+++ ddd/src/infrastructure/order/repository/sequilize/order.repository.ts
@@ -1,18 +1,10 @@
 import Order from "../../../../domain/checkout/entity/order";
+import OrderItem from "../../../../domain/checkout/entity/order_item";
 import OrderRepositoryInterface from "../../../../domain/checkout/repository/order-repository.interface";
 import OrderItemModel from "./order-item.model";
 import OrderModel from "./order.model";
 
 export default class OrderRepository implements OrderRepositoryInterface {
-  update(entity: Order): Promise<void> {
-    throw new Error("Method not implemented.");
-  }
-  find(id: string): Promise<Order> {
-    throw new Error("Method not implemented.");
-  }
-  findAll(): Promise<Order[]> {
-    throw new Error("Method not implemented.");
-  }
   async create(entity: Order): Promise<void> {
     await OrderModel.create(
       {
@@ -31,5 +23,34 @@ export default class OrderRepository implements OrderRepositoryInterface {
         include: [{ model: OrderItemModel }],
       }
     );
+  }
+
+  async find(id: string): Promise<Order> {
+    const orderModel = await OrderModel.findByPk(id, {
+      include: [{ model: OrderItemModel }],
+    });
+
+    return new Order(
+      orderModel.id,
+      orderModel.customer_id,
+      orderModel.items.map(
+        (item) =>
+          new OrderItem(
+            item.id,
+            item.name,
+            item.price,
+            item.product_id,
+            item.quantity
+          )
+      )
+    );
+  }
+
+  findAll(): Promise<Order[]> {
+    throw new Error("Method not implemented.");
+  }
+
+  update(entity: Order): Promise<void> {
+    throw new Error("Method not implemented.");
   }
 }
